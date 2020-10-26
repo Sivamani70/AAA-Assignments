d0d15746564694ae08f050b170f342b512b8baba
sivamani70 <siva153335@gmail.com>
2020-10-26 19:52:42 +0530

    Two Assignments Completed.

--- firebaseauth/src/main/java/com/example/firebaseauth/HomeScreen.java
+++ firebaseauth/src/main/java/com/example/firebaseauth/HomeScreen.java
@@ -1,7 +1,13 @@
 package com.example.firebaseauth;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -18,12 +24,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class HomeScreen extends AppCompatActivity {
     FirebaseAuth auth;
     ActivityHomeScreenBinding binding;
-    ProgressDialog dialog;
+    ProgressDialog progressDialog;
+    private DataModel dataModel;
 
 
     @Override
@@ -36,20 +45,17 @@ public class HomeScreen extends AppCompatActivity {
 //        actionBar.setDisplayHomeAsUpEnabled(true);
 
 
-        dialog = new ProgressDialog(this);
-        dialog.setMessage("Loading data...");
-        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        dialog.setCancelable(false);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading data...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCancelable(false);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home_screen);
 
         auth = FirebaseAuth.getInstance();
         fetchData();
-        dialog.show();
+        progressDialog.show();
 
-        binding.logout.setOnClickListener(v -> {
-            auth.signOut();
-            finish();
-        });
+
     }
 
 
@@ -68,12 +74,13 @@ public class HomeScreen extends AppCompatActivity {
         final String[] key = Objects.requireNonNull(Objects.requireNonNull(auth.getCurrentUser()).getEmail()).split("@");
         final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("userData");
         Query query = reference.orderByChild(Fields.USERNAME).equalTo(key[0]);
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+        query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 StringBuilder builder = new StringBuilder();
                 for (DataSnapshot data : snapshot.getChildren()) {
                     DataModel model = data.getValue(DataModel.class);
+                    HomeScreen.this.dataModel = model;
                     assert model != null;
                     builder.append("Name :: ").append(model.getNAME()).append("\n")
                             .append("User Name :: ").append(model.getUSERNAME()).append("\n")
@@ -87,14 +94,85 @@ public class HomeScreen extends AppCompatActivity {
                             .append("Gender :: ").append(model.getGENDER());
                 }
                 binding.data.setText(builder.toString());
-                dialog.dismiss();
+                progressDialog.dismiss();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                dialog.dismiss();
+                progressDialog.dismiss();
                 Toast.makeText(HomeScreen.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        final int edit = R.id.editData;
+        final int logout = R.id.signOut;
+        switch (item.getItemId()) {
+            case edit: {
+                upDateData();
+                break;
+            }
+            case logout: {
+                auth.signOut();
+                finish();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    void upDateData() {
+        View view = LayoutInflater.from(HomeScreen.this).inflate(R.layout.edit_data, null, false);
+
+        final EditText userName = view.findViewById(R.id.userName);
+        final EditText email = view.findViewById(R.id.email);
+        final EditText rollNumber = view.findViewById(R.id.rollNumber);
+        final EditText name = view.findViewById(R.id.personName);
+        final EditText number = view.findViewById(R.id.phoneNumber);
+        final EditText yearOfPass = view.findViewById(R.id.yearOfPass);
+        final EditText eBranch = view.findViewById(R.id.branch);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomeScreen.this);
+        builder.setView(view);
+        builder.setCancelable(false);
+        userName.setText(dataModel.getUSERNAME());
+        email.setText(dataModel.getMAIL());
+        rollNumber.setText(dataModel.getROLL_NUMBER());
+        name.setText(dataModel.getNAME());
+        number.setText(dataModel.getPHONE_NUMBER());
+        yearOfPass.setText(dataModel.getYEAR());
+        eBranch.setText(dataModel.getBRANCH());
+
+        userName.setEnabled(false);
+        email.setEnabled(false);
+        rollNumber.setEnabled(false);
+        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+        builder.setPositiveButton("UpDate", (dialog, which) -> {
+            progressDialog.show();
+            Map<String, Object> objectMap = new HashMap<>();
+            objectMap.put(Fields.NAME, String.valueOf(name.getText()));
+            objectMap.put(Fields.PHONE_NUMBER, String.valueOf(number.getText()));
+            objectMap.put(Fields.YEAR, String.valueOf(yearOfPass.getText()));
+            objectMap.put(Fields.BRANCH, String.valueOf(eBranch.getText()));
+            final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("userData");
+            reference.child(String.valueOf(userName.getText())).updateChildren(objectMap)
+                    .addOnSuccessListener(aVoid -> {
+                        Toast.makeText(HomeScreen.this, "Data Updated Sucessfully", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    }).addOnFailureListener(Throwable::printStackTrace);
+        });
+
+        builder.show();
+
     }
 }
